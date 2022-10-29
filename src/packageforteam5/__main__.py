928183a7c9e51cca5d2549004fbbb06f2cda48db
cty288 <tc3158@nyu.edu>
2022-10-29 16:34:09 -0400

    File Read (support default value and overridden path)

--- src/packageforteam5/__main__.py
+++ src/packageforteam5/__main__.py
@@ -1,10 +1,17 @@
 import wisdom
-
+import loader
+import os
 
 def main():
   line = wisdom.get()
   print(line)
+  print(loader.DEFAULT_PATH)
+  if not os.path.exists(os.path.dirname(loader.DEFAULT_PATH)):
+      os.makedirs(os.path.dirname(loader.DEFAULT_PATH))
 
+  print(loader.load("quiz")["sport"]["q1"]["options"][0])
+  print(loader.load("test"))
+  print(loader.load("notexist", "default"))
 if __name__ == '__main__':
     main()
 
---
+++ src/packageforteam5/loader.py
@@ -0,0 +1,26 @@
+import json
+import os
+from os.path import dirname, join
+
+DEFAULT_PATH = join(dirname(dirname(__file__)), 'data', 'save.json')
+
+@staticmethod
+def load(key, default_value=None, path=DEFAULT_PATH):
+    """_summary_
+
+    Args:
+        key (string): the key in the json file to load
+        default_value (object, optional): The default value to return if the key or the file does not exist. Defaults to None.
+        path (string, optional): the path of the saved Json file to load. Defaults to DEFAULT_PATH.
+
+    Returns:
+        object: the value of the key in the json file, or the default value if the key or the file does not exist
+    """
+    if not os.path.exists(path):
+        return default_value
+    with open(path) as f:
+        data = json.load(f)
+    value = data.get(key, default_value)
+    if(value == None):
+        return default_value
+    return value
