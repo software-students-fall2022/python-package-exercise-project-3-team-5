8ff39cf8f5f8907d6fa17469ca901bf3d0b67c97
okkiris <pq286@nyu.edu>
2022-11-01 16:49:15 -0400

    delete test not completed

---
+++ src/packageforteam5/tests/test_delete_path.py
@@ -0,0 +1,20 @@
+import json
+import os
+from os.path import dirname, join
+import sys
+import delete_path
+import default_path as path
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#import delete_path
+#import default_path as path
+
+def delete_existing_data():
+    path.setDefaultPath("w", join(dirname(dirname(dirname(__file__))), 'data', 'save_test.json'))
+    print(delete_path.delete("test"))
+
+
+def test_load_existing_data():
+    assert delete_existing_data() == None
+    
