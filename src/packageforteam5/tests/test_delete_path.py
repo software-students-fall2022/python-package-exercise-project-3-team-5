598d939c612d17f42a0d7b4233836a92d0aeba4b
okkiris <pq286@nyu.edu>
2022-11-01 19:22:25 -0400

    test delete

--- src/packageforteam5/tests/test_delete_path.py
+++ src/packageforteam5/tests/test_delete_path.py
@@ -2,19 +2,44 @@ import json
 import os
 from os.path import dirname, join
 import sys
-import delete_path
-import default_path as path
+
 
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-#import delete_path
-#import default_path as path
+import delete_path
+import default_path as path
 
-def delete_existing_data():
-    path.setDefaultPath("w", join(dirname(dirname(dirname(__file__))), 'data', 'save_test.json'))
+def delete_nonexisting_data():
+    path.setDefaultPath("r", join(dirname(dirname(dirname(__file__))), 'data', 'save.json'))
     print(delete_path.delete("test"))
 
+def delete_existing_data1():
+    path.setDefaultPath("r", join(dirname(dirname(dirname(__file__))), 'data', 'save_test.json'))
+    return delete_path.delete("quiz")
 
-def test_load_existing_data():
-    assert delete_existing_data() == None
-    
+def delete_existing_data2():
+    path.setDefaultPath("r", join(dirname(dirname(dirname(__file__))), 'data', 'save_test.json'))
+    return delete_path.delete("test")
+
+def delete_nonexisting_data2():
+    path.setDefaultPath("r", join(dirname(dirname(dirname(__file__))), 'data', 'save_test_2.json'))
+    return delete_path.delete("test")
+
+def delete_existing_data3():
+    path.setDefaultPath("r", join(dirname(dirname(dirname(__file__))), 'data', 'save_test_2.json'))
+    return delete_path.delete("glossary")
+
+def test_delete_nonexisting_data():
+    assert delete_nonexisting_data() == None
+
+def test_delete_existing_data1():
+    assert delete_existing_data1() == {"test" : 5}
+
+def test_delete_existing_data2():
+    assert delete_existing_data2() == {}  
+
+def test_delete_nonexisting_data2():
+    assert delete_nonexisting_data2() == None 
+
+def test_delete_existing_data3():
+    assert delete_existing_data3() == {}  
