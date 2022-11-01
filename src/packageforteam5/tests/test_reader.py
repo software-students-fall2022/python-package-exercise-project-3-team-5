d894ad7669882ea65260dd27db6bcb61f821b823
cty288 <tc3158@nyu.edu>
2022-11-01 19:44:54 -0400

    loader change

--- src/packageforteam5/tests/test_reader.py
+++ src/packageforteam5/tests/test_reader.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from types import SimpleNamespace
 import loader
 import default_path as path
+
     
 
 def load_existing_data():
